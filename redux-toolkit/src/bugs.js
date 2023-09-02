046abdd1144dcfe4ee37eddfac3e1b5d23ce04f2
Laxman B <laxmanbalaraman@gmail.com>
2023-09-02 16:38:45 +0530

    Create Slice in redux toolkit

--- redux-toolkit/src/bugs.js
+++ redux-toolkit/src/bugs.js
@@ -1,35 +1,74 @@
-import {createAction, createReducer} from '@reduxjs/toolkit'
 
-// creating actions
-export const bugAdded = createAction('bugAdded');
-export const bugRemoved = createAction('bugRemoved');
-export const bugUpdated = createAction('bugUpdated');
+// using createSlice for redux-toolkit that creates all actions and reducers in one go.
+
+import { createSlice } from "@reduxjs/toolkit";
+
 
 let lastId = 0;
 
-export default createReducer([], {
+const slice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducers: {
+        bugAdded : (bugs, action) => {
+            bugs.push({
+                id: ++lastId,
+                description: action.payload.description,
+                resolve: false
+            })
+        },
 
-    [bugAdded.type] : (bugs, action) => {
-        bugs.push({
-            id: ++lastId,
-            description: action.payload.description,
-            resolve: false
-        })
-    },
+        bugUpdated: (bugs, action) => {
+            const index = bugs.findIndex( bug => bug.id === action.payload.id);
+            bugs[index].description = action.payload.description;
+        },
 
-    [bugUpdated.type]: (bugs, action) => {
-        const index = bugs.findIndex( bug => bug.id === action.payload.id);
-        bugs[index].description = action.payload.description;
-    },
-
-    [bugRemoved.type] : (bugs, action) => {
-        const index = bugs.findIndex(bug => bug.id == action.payload.id);
-        index != -1 && bugs.splice(index, 1);
+        bugRemoved : (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id == action.payload.id);
+            index != -1 && bugs.splice(index, 1);
+        }
     }
+})
 
- })
 
-// a reducer takes two params: a state, a action
+export const {bugAdded, bugUpdated, bugRemoved} = slice.actions;
+export default slice.reducer;
+
+// using redux-toolkit to create actions and reducers
+
+// import {createAction, createReducer} from '@reduxjs/toolkit'
+
+// // creating actions
+// export const bugAdded = createAction('bugAdded');
+// export const bugRemoved = createAction('bugRemoved');
+// export const bugUpdated = createAction('bugUpdated');
+
+// let lastId = 0;
+
+// export default createReducer([], {
+
+//     [bugAdded.type] : (bugs, action) => {
+//         bugs.push({
+//             id: ++lastId,
+//             description: action.payload.description,
+//             resolve: false
+//         })
+//     },
+
+//     [bugUpdated.type]: (bugs, action) => {
+//         const index = bugs.findIndex( bug => bug.id === action.payload.id);
+//         bugs[index].description = action.payload.description;
+//     },
+
+//     [bugRemoved.type] : (bugs, action) => {
+//         const index = bugs.findIndex(bug => bug.id == action.payload.id);
+//         index != -1 && bugs.splice(index, 1);
+//     }
+
+//  })
+
+
+// creating our own reducer from scratch : a reducer takes two params: a state, a action
 // let lastId = 0;
 
 // export default function reducer(state = [] , action){
