fa969818817058f881ce907562cefcde8159539f
Laxman B <laxmanbalaraman@gmail.com>
2023-09-02 14:19:14 +0530

    configure store

--- redux-toolkit/src/store.js
+++ redux-toolkit/src/store.js
@@ -1,7 +1,7 @@
-import reducer from "./reducer";
-import  {createStore} from 'redux';
+import reducer from "./bugs";
+import  {configureStore} from '@reduxjs/toolkit'
 
 
-const store = createStore(reducer);
+export default configureStore({reducer})
 
-export default store;
+
