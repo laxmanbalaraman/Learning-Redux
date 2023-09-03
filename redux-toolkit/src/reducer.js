51d6071afc275a770523f244ae4745ef68d29c61
Laxman B <laxmanbalaraman@gmail.com>
2023-09-03 23:12:28 +0530

    combine reducers/ hierarchical reducers

--- redux-toolkit/src/reducer.js
+++ redux-toolkit/src/reducer.js
@@ -1,30 +1,7 @@
-// a reducer takes two params: a state, a action
-
-import * as actions from './actionTypes'; 
-
-let lastId = 0;
-
-export default function reducer(state = [] , action){
-
-    if(action.type === actions.BUG_ADDED)
-    return [
-        ...state, 
-        {
-            id: ++lastId,
-            description: action.payload.description,
-            resolve: false
-        }
-    ]
-
-    else if (action.type === actions.BUG_REMOVED)
-        return state.filter( bug => bug.id !== action.payload.id);
-
-    else if (action.type == actions.BUG_UPDATED){
-        return state.map(bug => bug.id !== action.payload.id ? bug : {...bug, description: action.payload.description}
-        )
-    }
+import { combineReducers } from "redux";
+import entityReducer from './entity'
 
 
-    return state;
-
-}
+export default combineReducers({
+    entity: entityReducer
+})
